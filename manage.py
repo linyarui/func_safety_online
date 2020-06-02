98e3b3cbdb988a2ee8855a12e21de4ac571ebe4d
linyarui <2237091767@qq.com>
2020-06-02 15:34:05 +0800

    给后台添加个admin管理员

--- manage.py
+++ manage.py
@@ -11,14 +11,15 @@ manager = Manager(app)
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
-# @manager.option('-u', '--username', dest='username')
-# @manager.option('-p', '--password', dest='password')
-# @manager.option('-e', '--email', dest='email')
-# def create_admin_user(username, password, email):
-#     user = BackendUser(username=username, password=password, email=email)
-#     db.session.add(user)
-#     db.session.commit()
-#     print('后台管理员用户创建成功')
+
+@manager.option('-u', '--username', dest='username')
+@manager.option('-p', '--password', dest='password')
+@manager.option('-e', '--email', dest='email')
+def create_admin_user(username, password, email):
+    user = BackendUser(username=username, password=password, email=email)
+    db.session.add(user)
+    db.session.commit()
+    print('后台管理员用户创建成功')
 
 
 if __name__ == '__main__':
