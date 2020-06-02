5fd43d82fc49856a5b5ed960d447795f4548b55d
linyarui <2237091767@qq.com>
2020-06-02 16:07:47 +0800

    后台修改密码

--- apps/backend/views.py
+++ apps/backend/views.py
@@ -1,10 +1,12 @@
-from flask import views, session, request
+from flask import views, session, request, g
 from flask import render_template, redirect, url_for
 from apps.backend import backend
 from apps.backend.models import BackendUser
-from apps.backend.forms import LoginForm
+from apps.backend.forms import LoginForm, ResetPwdForm
 from apps.backend.decorators import login_required
+from apps.utils import xjson
 from config import config
+from exts import db
 
 
 class IndexView(views.MethodView):
@@ -70,7 +72,32 @@ class ProfileView(views.MethodView):
         return render_template('backend/b_profile.html')
 
 
+class ResetPwdView(views.MethodView):
+    """
+    修改密码
+    """
+    decorators = [login_required]
+
+    def get(self):
+        return render_template('backend/b_resetpwd.html')
+
+    def post(self):
+        reset_pwd_form = ResetPwdForm(request.form)
+        if reset_pwd_form.validate():
+            old_pwd = reset_pwd_form.old_pwd.data
+            new_pwd = reset_pwd_form.new_pwd.data
+            user = g.cms_user
+            if user.check_password(old_pwd):
+                user.password = new_pwd
+                db.session.commit()
+                return xjson.json_success('密码修改成功')
+            return xjson.json_params_error('原密码输入错误')
+        message = reset_pwd_form.get_error()
+        return xjson.json_params_error(message)
+
+
 backend.add_url_rule('/index/', view_func=IndexView.as_view('index'))
 backend.add_url_rule('/login/', view_func=LoginView.as_view('login'))
 backend.add_url_rule('/logout/', view_func=LogoutView.as_view('logout'))
 backend.add_url_rule('/profile/', view_func=ProfileView.as_view('profile'))
+backend.add_url_rule('/resetpwd/', view_func=ResetPwdView.as_view('resetpwd'))
---
+++ apps/static/backend/js/b_resetpwd.js
@@ -0,0 +1,42 @@
+$(function () {
+  $('#submit').click(function (event) {
+    //阻止按钮默认的提交表单行为
+    event.preventDefault();
+    var oldpwdE = $('input[name=old_pwd]');
+    var newpwdE = $('input[name=new_pwd]');
+    var newpwd2E = $('input[name=new_pwd2]');
+
+    var oldpwd = oldpwdE.val();
+    var newpwd = newpwdE.val();
+    var newpwd2 = newpwd2E.val();
+
+    //这里使用我们自己封装好的bbsajax，它具有了csrf
+    iqajax.post({
+      'url': '/backend/resetpwd/',
+      'data': {
+        'old_pwd': oldpwd,
+        'new_pwd': newpwd,
+        'new_pwd2': newpwd2
+      },
+      'success': function (data) {
+        //根据状态码判断
+        if (data['code'] === 200) {
+          //弹出成功的提示框，提示语是从后台传过来的message
+          xtalert.alertSuccessToast(data['message']);
+          oldpwdE.val('');   //完成请求后把表单输入的值清空
+          newpwdE.val('');
+          newpwd2E.val('');
+          window.location.href = '/backend/login/'
+        } else {
+          xtalert.alertError(data['message']);
+          oldpwdE.val('');
+          newpwdE.val('');
+          newpwd2E.val('');
+        }
+      },
+      'fail': function (error) {
+        xtalert.alertNetworkError('网络错误');
+      }
+    });
+  });
+});
